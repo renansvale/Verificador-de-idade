ea1f88236a9657fecd5a517c9ed646c531cf9c0c
renansvale <64032043+renansvale@users.noreply.github.com>
2021-04-29 16:32:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+  namespace myapp
+  {
+      class Program
+      {
+          static void Main(string[] args)
+          {
+              int idade; //declaração da variável idade como inteiro
+              Console.WriteLine("Informe sua idade:");
+              idade = int.Parse(Console.ReadLine()); //leitura da idade e conversão em inteiro
+              if(idade>=18) //teste condicional
+                      Console.WriteLine("Você tem idade para tirar carteira de habilitação."); //mensagem se condição for verdadeira
+              else
+                      Console.WriteLine("Você não tem idade para tirar carteira de motorista");
+          }
+      }
+  }
